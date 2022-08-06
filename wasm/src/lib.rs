0ca69ba4fe1e21f469d5c68e43464b1e28b87520
rana <minami.smd@gmail.com>
2022-08-07 00:36:28 +0900

    define alias for WebGl2RenderingContext

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,21 +1,18 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{ WebGl2RenderingContext, WebGlShader, WebGlProgram };
+use web_sys::{ WebGl2RenderingContext as GL, WebGlShader, WebGlProgram };
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 
-    let document = web_sys::window().unwrap()
-        .document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap()
-        .dyn_into::<web_sys::HtmlCanvasElement>()?;
-    let gl = canvas.get_context("webgl2")?.unwrap()
-        .dyn_into::<WebGl2RenderingContext>()?;
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap().dyn_into::<web_sys::HtmlCanvasElement>()?;
+    let gl = canvas.get_context("webgl2")?.unwrap().dyn_into::<GL>()?;
 
-    let vertex_shader = create_shader(&gl, WebGl2RenderingContext::VERTEX_SHADER, include_str!("shader/vertex_shader.glsl"))?;
-    let fragment_shader = create_shader(&gl, WebGl2RenderingContext::FRAGMENT_SHADER, include_str!("shader/fragment_shader.glsl"))?;
+    let vertex_shader = create_shader(&gl, GL::VERTEX_SHADER, include_str!("shader/vertex.glsl"))?;
+    let fragment_shader = create_shader(&gl, GL::FRAGMENT_SHADER, include_str!("shader/fragment.glsl"))?;
 
     let program = create_program(&gl, &vertex_shader, &fragment_shader)?;
     gl.use_program(Some(&program));
@@ -26,16 +23,13 @@ pub fn start() -> Result<(), JsValue> {
     let pos_buffer = gl.create_buffer().ok_or("Failed to create buffer")?;
     let color_buffer = gl.create_buffer().ok_or("Failed to create buffer")?;
 
-    // let vao = context.create_vertex_array().ok_or("Failed to create vertex array object")?;
-    // context.bind_vertex_array(Some(&vao));
-
-    gl.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&pos_buffer));
+    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&pos_buffer));
     gl.enable_vertex_attrib_array(pos_location);
-    gl.vertex_attrib_pointer_with_i32(pos_location, VERTEX_SIZE as i32, WebGl2RenderingContext::FLOAT, false, 0, 0);
+    gl.vertex_attrib_pointer_with_i32(pos_location, VERTEX_SIZE as i32, GL::FLOAT, false, 0, 0);
 
-    gl.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&color_buffer));
+    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&color_buffer));
     gl.enable_vertex_attrib_array(color_location);
-    gl.vertex_attrib_pointer_with_i32(color_location, COLOR_SIZE as i32, WebGl2RenderingContext::FLOAT, false, 0, 0);
+    gl.vertex_attrib_pointer_with_i32(color_location, COLOR_SIZE as i32, GL::FLOAT, false, 0, 0);
 
     const VERTEX_SIZE: usize = 3;
     const COLOR_SIZE: usize = 4;
@@ -58,72 +52,52 @@ pub fn start() -> Result<(), JsValue> {
         0.0, 0.0, 1.0, 1.0,
     ];
 
-    gl.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&pos_buffer));
+    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&pos_buffer));
     unsafe {
         let pos_arr_buf_view = js_sys::Float32Array::view(&vertices);
-        gl.buffer_data_with_array_buffer_view(
-            WebGl2RenderingContext::ARRAY_BUFFER,
-            &pos_arr_buf_view,
-            WebGl2RenderingContext::STATIC_DRAW,
-        );
+        gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &pos_arr_buf_view, GL::STATIC_DRAW);
     }
 
-    gl.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&color_buffer));
+    gl.bind_buffer(GL::ARRAY_BUFFER, Some(&color_buffer));
     unsafe {
         let color_arr_buf_view = js_sys::Float32Array::view(&colors);
-        gl.buffer_data_with_array_buffer_view(
-            WebGl2RenderingContext::ARRAY_BUFFER,
-            &color_arr_buf_view,
-            WebGl2RenderingContext::STATIC_DRAW,
-        );
+        gl.buffer_data_with_array_buffer_view(GL::ARRAY_BUFFER, &color_arr_buf_view, GL::STATIC_DRAW);
     }
-
-    // context.bind_vertex_array(Some(&vao));
-    // context.viewport(0, 0, canvas.width() as i32, canvas.height() as i32);
 
     draw(&gl, VERTEX_COUNT as i32);
 
     Ok(())
 }
 
-fn create_shader(
-    gl: &WebGl2RenderingContext,
-    shader_type: u32,
-    source: &str
-) -> Result<WebGlShader, String> {
+fn create_shader(gl: &GL, shader_type: u32, source: &str) -> Result<WebGlShader, String> {
     let shader = gl.create_shader(shader_type).ok_or_else(|| String::from("Failed to create shader object"))?;
-
     gl.shader_source(&shader, source);
     gl.compile_shader(&shader);
 
-    if gl.get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS).as_bool().unwrap_or(false) {
+    if gl.get_shader_parameter(&shader, GL::COMPILE_STATUS).as_bool().unwrap_or(false) {
         Ok(shader)
     } else {
         Err(gl.get_shader_info_log(&shader).unwrap_or_else(|| String::from("Failed to compile shader")))
     }
 }
 
-fn create_program(
-    gl: &WebGl2RenderingContext,
-    vertex_shader: &WebGlShader,
-    fragment_shader: &WebGlShader,
-) -> Result<WebGlProgram, String> {
+fn create_program(gl: &GL, vertex_shader: &WebGlShader, fragment_shader: &WebGlShader) -> Result<WebGlProgram, String> {
     let program = gl.create_program().ok_or_else(|| String::from("Failed to create program object"))?;
     gl.attach_shader(&program, &vertex_shader);
     gl.attach_shader(&program, &fragment_shader);
     gl.link_program(&program);
 
-    if gl.get_program_parameter(&program, WebGl2RenderingContext::LINK_STATUS).as_bool().unwrap_or(false) {
+    if gl.get_program_parameter(&program, GL::LINK_STATUS).as_bool().unwrap_or(false) {
         Ok(program)
     } else {
         Err(gl.get_program_info_log(&program).unwrap_or_else(|| String::from("Failed to link program")))
     }
 }
 
-fn draw(gl: &WebGl2RenderingContext, vertex_count: i32) {
+fn draw(gl: &GL, vertex_count: i32) {
     gl.clear_color(0.0, 0.0, 0.0, 1.0);
-    gl.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
+    gl.clear(GL::COLOR_BUFFER_BIT);
 
-    gl.draw_arrays(WebGl2RenderingContext::TRIANGLES, 0, vertex_count);
+    gl.draw_arrays(GL::TRIANGLES, 0, vertex_count);
     gl.flush();
 }
